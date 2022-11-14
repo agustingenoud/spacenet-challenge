3c703bd79654d8b7a5664988d38db628ad67afea
agustingenoud <agustingenoud@gmail.com>
2022-11-14 07:40:06 -0300

    control-env--bash-scripting

--- control-env.sh
+++ control-env.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 function start {
   echo 'Starting Docker environment . . .'
-  docker-compose build spacenet-notebooks && docker-compose --project-name spacenet7 up -d && webcall 
+  #docker-compose build spacenet-notebooks && docker-compose --project-name spacenet7 up -d && webcall
+  exists
 }
-
 
 function webcall {
   sleep 5 && python3 -m webbrowser http://localhost:8080
@@ -18,6 +18,21 @@ function cleanup {
   echo "Removing volume"
   docker volume rm earth_postgres-data
   docker volume rm earth_superset
+}
+
+function exists {
+  echo "Checking existence of docker image..."
+  local IMAGE="$(docker image inspect -f '{{.Id}}' challenge-spacenet-notebooks)"
+  if [ -f "$IMAGE" ]; then
+    echo "Image exist, runnning container..."
+    docker-compose --project-name spacenet7 up -d && webcall
+  else
+    echo "Image don't exist, building image..."
+    docker-compose build spacenet-notebooks
+    echo "Image build successful, running container..."
+    docker-compose --project-name spacenet7 up -d && webcall
+  fi
+
 }
 
 case $1 in
@@ -36,6 +51,10 @@ stop)
 
 cleanup)
   cleanup
+  ;;
+
+exists)
+  exists
   ;;
 
 *)
